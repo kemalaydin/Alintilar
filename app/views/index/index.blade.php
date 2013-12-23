bb546e60e3540edeef5b450f8a1a75b10711e985
kemalaydin <kemal@whianex.com>
2013-12-23 19:04:47 +0200

    Ana sayfa üzerinde bir kaç grafik değişikliği ve logo düzenlemesi

--- app/views/index/index.blade.php
+++ app/views/index/index.blade.php
@@ -13,7 +13,13 @@
 
                   <a href="{{ url('quote/'.$lastQuote->id ) }}"  class="list-group-item">
                   <h5 class="list-group-item-heading">{{  $lastQuote->quote }}</h5>
-                  <small class="list-group-item-text"><b> {{ $lastQuote->Person->name }} </b> 'dan alıntı yapıldı <span class="label label-info">( Kaynak : <b> {{ $lastQuote->Product->name }} </b> )</span></small>
+                  <small class="list-group-item-text"><b> {{ $lastQuote->Person->name }} </b> 'dan alıntı yapıldı <span class="green-color">( Kaynak : <b> {{ $lastQuote->Product->name }} </b> )</span></small>
+
+                  <div style="float:right;">
+                  	<span class="label label-primary">Facebook'ta Paylaş</span>
+                  	<span class="label label-info">Tweetle</span>
+
+                  </div>
                	 </a>
 
                 @endforeach
