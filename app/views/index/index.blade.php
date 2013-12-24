b9d9121b68a109bad649ea8afc61232a40a33eab
kemalaydin <kemal@whianex.com>
2013-12-24 18:21:55 +0200

    kemal vendor silme2

--- app/views/index/index.blade.php
+++ app/views/index/index.blade.php
@@ -14,18 +14,37 @@
                   <a href="{{ url('quote/'.$lastQuote->id ) }}"  class="list-group-item">
                   <h5 class="list-group-item-heading">{{  $lastQuote->quote }}</h5>
                   <small class="list-group-item-text"><b> {{ $lastQuote->Person->name }} </b> 'dan alıntı yapıldı <span class="green-color">( Kaynak : <b> {{ $lastQuote->Product->name }} </b> )</span></small>
+                   	    <div style="float:right;">
+          
 
-                  <div style="float:right;">
-                  	<span class="label label-primary">Facebook'ta Paylaş</span>
+            <!--       	<a target="_blank" href="http://www.facebook.com/sharer.php?s=100
+					&p[url]={{ url('quote/'.$lastQuote->id ) }}
+					&p[title]={{ $lastQuote->quote }}
+					&p[summary]={{ $lastQuote->Person->name }} 'dan alıntı yapıldı ( Kaynak : {{ $lastQuote->Product->name }} )"  -->
+
+					<label class="label label-primary">Facebook'ta Paylaş</label>
+
+                  	
                   	<span class="label label-info">Tweetle</span>
 
                   </div>
+               
                	 </a>
+              
+                  <div style="clear:both;"></div>
 
                 @endforeach
+
+                @foreach($People as $Person)
+
+                	{{ $Person->name }}
+
+                @endforeach
+
+                
                   
         </div>
-
+                
 
 </div>
 @stop
